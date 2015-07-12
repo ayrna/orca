d3a47f6832987ea3291778a969fe4a6a331d5006
Javier SM <jsanchezm@uco.es>
2015-07-12 18:22:31 +0200

    Added HTCondor scripts
    Added configuration files examples

---
+++ src/condor/condor-joinResults.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Check arguments
+if [ $# != 1 ]; then
+	echo "Usage:" $0 "name-of-the-directory"
+	exit 127
+fi
+
+if [ -f "./Experiments/exp-$1/Results/results-$1-complete.tar.gz" ]
+then
+	rm "./Experiments/exp-$1/Results/results-$1-complete.tar.gz";
+fi
+
+if [ -f "./Experiments/exp-$1/Results/mean-results.csv" ]
+then
+	rm "./Experiments/exp-$1/Results/mean-results.csv";
+fi
+
+if [ -f "./Experiments/exp-$1/experiments.tar.gz" ]
+then
+	rm "./Experiments/exp-$1/experiments.tar.gz";
+fi
+
+cmd="/usr/local/matlab/bin/matlab -nodesktop -nojvm -nodisplay -r addpath('..');Utilities.results('Experiments/exp-$1/Results');quit";
+$cmd;
+
+
+cd ./Experiments/exp-$1/Results/
+tar zcvf results-$1-complete.tar.gz */
+rm -Rf ./*/
+cd ../../../
+
+cd ./Experiments/exp-$1/
+tar zcvf experiments.tar.gz exp-*
+rm -Rf exp-*
+cd ../../
+
+cd $1
+tar zcvf errorsCreateRun.tar.gz condor-createExperiments.err condor-runExperiments-*.err
+rm condor-createExperiments.err condor-runExperiments-*.err
+tar zcvf outputsCreateRun.tar.gz condor-createExperiments.out condor-runExperiments-*.out
+rm condor-createExperiments.out condor-runExperiments-*.out
---
+++ src/condor/condor-runExperiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Check arguments
+if [ $# != 2 ]; then
+	echo "Usage:" $0 "name-of-the-directory number-of-file-to-run"
+	exit 127
+fi
+hostname
+
+EXPERIMENTS=(`ls Experiments/exp-$1/exp*`);
+
+echo "Launching ${EXPERIMENTS[$2]} configuration file (number $2)!!"
+
+cmd="/usr/local/matlab/bin/matlab -nodesktop -nojvm -nodisplay -r addpath('..');Utilities.runExperiment('${EXPERIMENTS[$2]}');quit";
+$cmd;
