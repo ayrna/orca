e869982c84a4e3fa816332be8a91737a4d9d61b7
Maria <i82perom@uco.es>
2015-07-12 19:18:13 +0200

    Documentation for the algorithm class

--- src/Algorithm.m
+++ src/Algorithm.m
@@ -1,63 +1,49 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (C) Pedro Antonio Gutiérrez (pagutierrez at uco dot es)
+% María Pérez Ortiz (i82perom at uco dot es)
+% Javier Sánchez Monedero (jsanchezm at uco dot es)
+%
+% This file contains the abstract class that defines the different functions and attributes for an algorithm class, presented in the paper Ordinal regression methods: survey and experimental study published in the IEEE Transactions on Knowledge and Data Engineering. 
+% 
+% The code has been tested with Ubuntu 12.04 x86_64, Debian Wheezy 8, Matlab R2009a and Matlab 2011
+% 
+% If you use this code, please cite the associated paper
+% Code updates and citing information:
+% http://www.uco.es/grupos/ayrna/orreview
+% https://github.com/ayrna/orca
+% 
+% AYRNA Research group's website:
+% http://www.uco.es/ayrna 
+%
+% This program is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 3
+% of the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
+% Licence available at: http://www.gnu.org/licenses/gpl-3.0.html
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 classdef Algorithm < handle
-    % Algorithm Abstract interface class
-    % Abstract class which defines Machine Learning algorithms.
-    % It describes some common methods and variables for all the
-    % algorithMs.
+    % Algorithm abstract interface class
+    % Abstract class which defines the settings for the algorithms (common methods and variables).
     
     properties
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %
-        % Variable: kernelType (Public)
-        % Type: String
-        % Description: It specifies the kernel function
-        %               used by the algorithm. Possible
-        %               values are: 'no', 'rbf',
-        %               'sigmoid', 'linear' or 'poly'
-        %
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         kernelType = 'rbf'
         name
         
     end
     
-    %     properties(Access = protected)
-    %
-    %         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %         %
-    %         % Variable: name (Protected)
-    %         % Type: String
-    %         % Description: This variables contains the name
-    %         %              of the implemented algorithm
-    %         %
-    %         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %
-    %         name
-    %
-    %     end
-    
-    
     methods
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %
-        % Function: set.kernelType (Public)
-        % Description: It verifies if the value for the
-        %               variable kernelType is correct.
-        % Type: Void
-        % Arguments:
-        %           value--> Value for the variable kernelType.
-        %
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-%         function obj = set.kernelType(obj, value)
-%             if ~(strcmpi(value,'no') || strcmpi(value,'rbf') || strcmpi(value,'sigmoid') || strcmpi(value,'poly') || strcmpi(value,'lin'))
-%                 error('Invalid value for Kernel type ');
-%             else
-%                 obj.kernelType = value;
-%             end
-%         end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %
@@ -76,14 +62,12 @@ classdef Algorithm < handle
         
     end
     
-    
     methods(Abstract)
+
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %
         % Function: runAlgorithm(Public)
-        % Description: This function is not implemented,
-        %                   since it will be designed by
-        %                   the derivated class.
+        % Description: function to run the algorithm (train and test partitions)
         % Type: Void
         % Arguments:
         %           No arguments
@@ -95,9 +79,7 @@ classdef Algorithm < handle
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %
         % Function: defaultParameters(Public)
-        % Description: This function is not implemented,
-        %                   since it will be designed by
-        %                   the derivated class.
+        % Description: function for setting the default parameters
         % Type: Void
         % Arguments:
         %           No arguments
@@ -109,9 +91,7 @@ classdef Algorithm < handle
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %
         % Function: train(Public)
-        % Description: This function is not implemented,
-        %                   since it will be designed by
-        %                   the derivated class.
+        % Description: function for training the model
         % Type: Void
         % Arguments:
         %           No arguments
@@ -123,9 +103,7 @@ classdef Algorithm < handle
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %
         % Function: test(Public)
-        % Description: This function is not implemented,
-        %                   since it will be designed by
-        %                   the derivated class.
+        % Description: function for testing the model
         % Type: Void
         % Arguments:
         %           No arguments
