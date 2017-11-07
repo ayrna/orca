5d7ac17e820cf9ac4c2fb856f0206616818e2401
Javier Sánchez <jsanchez@uloyola.es>
2017-11-07 13:26:02 +0100

    Added test of scripts with x-hold-out design

--- src/runtestscv.m
+++ src/runtestscv.m
@@ -1,25 +1,46 @@
+% This script runs full experiment test using the ORCA scripts to describe and
+% experiment. The toy dataset is used with a 1 or 30-hold-out experimental
+% design and method's parameter optimization. At the end of the test there
+% should be one new folder for each method in the 'Experiments' directory. 
+% The following performamnce reports should appear in
+% 'Experiments/exp-<id>/Results/'
+% 
+% - mean-results_matrices_sum_test.csv
+% - mean-results_matrices_sum_train.csv
+% - mean-results_test.csv
+% - mean-results_train.csv
+% 
 % To run an individual test just use the right function call
 %
-% Utilities.runExperiments('tests/cvtests/cssvc')
-% Utilities.runExperiments('tests/cvtests/elmop')
-% Utilities.runExperiments('tests/cvtests/kdlor')
-% Utilities.runExperiments('tests/cvtests/orboostall')
-% Utilities.runExperiments('tests/cvtests/pom')
-% Utilities.runExperiments('tests/cvtests/redsvm')
-% Utilities.runExperiments('tests/cvtests/svc1v1')
-% Utilities.runExperiments('tests/cvtests/svc1va')
-% Utilities.runExperiments('tests/cvtests/svmop')
-% Utilities.runExperiments('tests/cvtests/svorex')
-% Utilities.runExperiments('tests/cvtests/svorim')
-% Utilities.runExperiments('tests/cvtests/svorimlin')
-% Utilities.runExperiments('tests/cvtests/svr')
+% Utilities.runExperiments('tests/cvtests-1-holdout/<script>')
+% Utilities.runExperiments('tests/cvtests-30-holdout/<script>')
 
-d = dir(['tests' filesep 'cvtests']);
+% Fast test
+tests_dir = ['tests' filesep 'cvtests-1-holdout'];
+% Long test 
+% tests_dir = ['tests' filesep 'cvtests-30-holdout'];
+files = dir(tests_dir);
 
 % Delete .. and .
-d(1)=[];
-d(1)=[];
+files(1)=[];
+files(1)=[];
 
-for i=1:length(d)
-    Utilities.runExperiments(['tests' filesep 'cvtests' filesep  d(1).name], true);
+for i=1:length(files)
+    exp_dir = Utilities.runExperiments([tests_dir filesep  files(i).name], false);
+    try 
+        %result_csv = csvread(['Experiments' filesep exp_dir filesep 'Results' filesep 'mean-results_test.csv'], 1,1);
+        csv_result = [exp_dir filesep 'Results' filesep 'mean-results_test.csv'];
+        %results = csvread(csv_result, 1,0);
+        results = csvread(csv_result, 1,1);
+        
+        % Check we have some numerical results. 
+        if sum(results) > 0
+            fprintf('Test passed for %s\n', files(i).name);
+        else
+            error('Test FAILED for %s. CSV files do not contain valid data. \n', files(i).name)
+        end
+    catch err
+        error('Test FAILED for "%s". Unable to open CSV results file', files(i).name)
+    end
+    
 end
