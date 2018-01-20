cceda2fc7558160001d21051b8967f08790f6018
pagutierrez <pagutierrez@uco.es>
2018-01-20 13:03:37 +0100

    Kfold for ERA

---
+++ src/code-examples/exampleERAKFold.m
@@ -0,0 +1,25 @@
+% Load data
+ERAData = table2array(readtable('../../exampledata/ERA.csv'));
+
+% Check the first 20 rows
+ERAData(1:20,:)
+
+% Extract targets
+targets = ERAData(:,end);
+
+% Generate k fold partitions
+k=10;
+CVO = cvpartition(targets,'k',k);
+
+% For each fold
+nameDataset = 'era';
+rootDir = fullfile('..', '..', 'exampledata', '10-fold', nameDataset);
+mkdir(rootDir);
+rootDir = fullfile(rootDir,'matlab');
+mkdir(rootDir);
+for ff = 1:CVO.NumTestSets
+    trIdx = CVO.training(ff);
+    teIdx = CVO.test(ff);
+    dlmwrite(fullfile(rootDir,sprintf('train-%s.%d',nameDataset,ff-1)),ERAData(trIdx,:),' ');
+    dlmwrite(fullfile(rootDir,sprintf('test-%s.%d',nameDataset,ff-1)),ERAData(teIdx,:),' ');
+end
