6a8a3b811012e6af1ee1aaed8cfcc1dee4c86819
Javier Sanchez <javism@posteo.net>
2019-04-17 14:56:14 +0200

    Uncommented generation of tutorial 1

--- doc/notebooks_to_html.sh
+++ doc/notebooks_to_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca-tutorial-1.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca-tutorial-1.ipynb
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca-tutorial-2.ipynb
 
