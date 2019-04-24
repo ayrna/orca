87f3f0c02af4137c1d0e1bdcb1d8e9ec3e8bb7ab
Javier Sanchez <javism@posteo.net>
2019-04-24 13:49:53 +0200

    Renamed tutorials

--- doc/test_notebooks.sh
+++ doc/test_notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jupyter nbconvert --to script orca-tutorial-1.ipynb
-octave-cli orca-tutorial-1.m
-jupyter nbconvert --to script orca-tutorial-2.ipynb
-octave-cli orca-tutorial-2.m
+jupyter nbconvert --to script orca_tutorial_1.ipynb
+octave-cli --eval "orca_tutorial_1"
+jupyter nbconvert --to script orca_tutorial_2.ipynb
+octave-cli --eval "orca_tutorial_2"
