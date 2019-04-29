aff3029e28aa9ddc23d5478e325f4d831d05b96d
Javier Sanchez <javism@posteo.net>
2019-04-29 22:46:05 +0200

    Added notebook 3 to html generation script

--- doc/notebooks_to_html.sh
+++ doc/notebooks_to_html.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca-tutorial-1.ipynb
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca-tutorial-2.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca_tutorial_1.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca_tutorial_2.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html_toc --execute orca_tutorial_3.ipynb
 
