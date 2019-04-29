16032a431c51590d0061f3273d94de4d62160951
Javier Sanchez <javism@posteo.net>
2019-04-29 19:22:02 +0200

    test moved to upper directory

--- doc/test_notebooks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-jupyter nbconvert --to script orca_tutorial_1.ipynb
-octave-cli --eval "orca_tutorial_1"
-jupyter nbconvert --to script orca_tutorial_2.ipynb
-octave-cli --eval "orca_tutorial_2"
