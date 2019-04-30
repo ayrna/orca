d4609d3185685498cb5a78eab9a260920b7542c3
Javier Sanchez <javism@posteo.net>
2019-04-30 09:44:16 +0200

    Merged installation scripts for Octave and Matlab in one file

---
+++ build_orca.m
@@ -0,0 +1,33 @@
+% This script builds ORCA for Octave/MATLAB. The script check dependencies and 
+% install required Octave packages if needed. In MATLAB, it looks for 
+% toolboxes.
+% The script runs basic tests after installation.%
+
+disp('== Check and install dependencies... ==')
+% Build ORCA for Octave and install dependencies
+if (exist ('OCTAVE_VERSION', 'builtin') > 0)
+	ip = pkg ("list", "statistics");
+	if length(ip) == 0
+		pkg install -forge io
+		pkg install -forge statistics
+	end
+	ip = pkg ("list", "optim");
+	if length(ip) == 0
+		pkg install -forge struct
+		pkg install -forge optim
+	end
+else % Build ORCA for MATLAB
+	if (license('test','statistics_toolbox') == 0)
+		error('ORCA needs the Statistics and Machine Learning Toolbox ')
+	end
+end
+
+disp('== Compile C/C++ source... ==')
+cd src/Algorithms
+make
+make clean
+cd ..
+
+disp('== Run basic tests... == ')
+
+runtestssingle
